128116939ea0be9128551ec0656742ae776ec40b
2vawulence <ahyiyann2@gmail.com>
2026-08-21 09:30:48 +0000

    Correction erreur d affichage

--- app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -16,13 +16,20 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dice_roller.R.drawable.dice_6
 import com.example.dice_roller.ui.theme.Dice_RollerTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,16 +50,27 @@ open class ComponentActivity {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+  var result by remember { mutableStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> dice_6
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier)
     {
         Image(
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription ="1"
+            painter = painterResource(imageResource),
+            contentDescription =result.toString()
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {}) {
+        Button(onClick = {
+            result = (1..6).random()
+        }) {
             Text(text = stringResource(R.string.Roll))
         }
     }
